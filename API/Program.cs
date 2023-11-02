0717d2fde7e0ab5b04d59701c9df1b702ad0fcc3
tanutv123 <taitanvo2309@gmail.com>
2023-11-02 16:04:43 +0700

    Complete walking skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
 	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//Add CORS
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,15 +21,21 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
+	
 	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
