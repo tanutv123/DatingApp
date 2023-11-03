f2fa23062487c58c5b302b3871701bd65cb88cf1
tanutv123 <taitanvo2309@gmail.com>
2023-11-03 16:55:43 +0700

    Add basic Authentication(not using ASP.NET Identity)

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using API.interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+		{
+			//Add DBContext
+			services.AddDbContext<DataContext>(options =>
+			{
+				options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+			});
+			//Add CORS
+			services.AddCors();
+			//Add Services
+			services.AddScoped<ITokenService, TokenService>();
+			return services;
+		}
+	}
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,18 @@
 using API.Data;
+using API.Extensions;
+using API.interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-
-//Add DBContext
-builder.Services.AddDbContext<DataContext>(options => 
-{
-	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-//Add CORS
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,6 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
